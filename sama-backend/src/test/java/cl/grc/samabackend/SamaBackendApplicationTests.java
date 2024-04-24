25ac4449a89ea563b9bb8610ba9dc9871625889a
nicolasgabrielli <nicolas.gabrielli@usach.cl>
2024-04-23 22:54:47 -0400

    Se inicializó el espacio de desarrollo para el springboot.

---
+++ sama-backend/src/test/java/cl/grc/samabackend/SamaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.grc.samabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
