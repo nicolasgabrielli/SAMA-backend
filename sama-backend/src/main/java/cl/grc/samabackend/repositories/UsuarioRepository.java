e1f004950e0e7f48dec1dc1dfca449db9315201e
nicolasgabrielli <nicolas.gabrielli@usach.cl>
2024-04-27 16:54:03 -0400

    Se crearon algunos entities y repositories

---
+++ sama-backend/src/main/java/cl/grc/samabackend/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package cl.grc.samabackend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioRepository, Integer>{
+    
+}
