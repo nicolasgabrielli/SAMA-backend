61d10fb14274a423a8fffc3da536cf5e8beecdc5
Ignacio Riquelme <ignacio.riquelme.r@usach.cl>
2024-06-09 13:30:28 -0400

    Tabla provisional de indicadores

--- src/main/java/sama/service/ReporteDocumentService.java
+++ src/main/java/sama/service/ReporteDocumentService.java
@@ -1,11 +1,9 @@
 package sama.service;
 
+import org.apache.poi.xwpf.usermodel.*;
 import com.aspose.words.Document;
 import com.aspose.words.SaveFormat;
-import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sama.entity.Reporte;
@@ -36,6 +34,8 @@ public class ReporteDocumentService {
         for (Categoria categoria : categorias) {
             agregarCategoria(document, categoria);
         }
+
+        addSummaryTable(document, reporte);
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         document.write(outputStream);
@@ -82,7 +82,7 @@ public class ReporteDocumentService {
     private void agregarCampo(XWPFDocument document, Campo campo) {
         XWPFParagraph campoParagraph = document.createParagraph();
         if (campo.getTitulo() != null) {
-            XWPFRun  tituloRun = campoParagraph.createRun();
+            XWPFRun tituloRun = campoParagraph.createRun();
             tituloRun.setText(campo.getTitulo());
             tituloRun.setBold(true);
             tituloRun.setFontSize(13);
@@ -91,10 +91,9 @@ public class ReporteDocumentService {
             XWPFRun contenidoRun = campoParagraph.createRun();
             contenidoRun.addBreak();
             if (!campo.getTipo().equals("tabla")) {
-                System.out.println("Entre aqui");
                 contenidoRun.setText(campo.getContenido().toString());
                 contenidoRun.setFontSize(11);
-            }else {
+            } else {
                 // TODO: Implementar tabla
             }
         }
@@ -108,7 +107,7 @@ public class ReporteDocumentService {
     private void agregarSubCampo(XWPFDocument document, Campo campo) {
         XWPFParagraph campoParagraph = document.createParagraph();
         if (campo.getTitulo() != null) {
-            XWPFRun  tituloRun = campoParagraph.createRun();
+            XWPFRun tituloRun = campoParagraph.createRun();
             tituloRun.setText(campo.getTitulo());
             tituloRun.setBold(true);
             tituloRun.setFontSize(12);
@@ -116,13 +115,88 @@ public class ReporteDocumentService {
         if (campo.getContenido() != null) {
             XWPFRun contenidoRun = campoParagraph.createRun();
             contenidoRun.addBreak();
-            if(!campo.getTipo().equals("tabla")){
+            if (!campo.getTipo().equals("tabla")) {
                 contenidoRun.setText(campo.getContenido().toString());
                 contenidoRun.setFontSize(11);
-            }
-            else {
+            } else {
                 // TODO: Implementar tabla
             }
+        }
+    }
+
+    private void addSummaryTable(XWPFDocument document, Reporte reporte) {
+        XWPFParagraph summaryTitle = document.createParagraph();
+        summaryTitle.setStyle("Heading1");
+        summaryTitle.setAlignment(ParagraphAlignment.LEFT);
+        XWPFRun titleRun = summaryTitle.createRun();
+        titleRun.setText("Tabla de indicadores");
+        titleRun.setBold(true);
+        titleRun.setFontSize(14);
+
+        // Crear la tabla
+        XWPFTable summaryTable = document.createTable();
+
+        // Establecer alineación centrada para todas las celdas
+        summaryTable.setRowBandSize(15);
+        summaryTable.setWidth("auto"); // Ajustar al tamaño del contenido
+        summaryTable.setCellMargins(200, 100, 200, 100);
+
+        // Crear fila de encabezado
+        XWPFTableRow headerRow = summaryTable.getRow(0);
+
+        // Crear las celdas necesarias
+        for (int i = 0; i < 3; i++) {
+            headerRow.addNewTableCell();
+        }
+
+        // Establecer el texto y el estilo de los encabezados
+        for (int i = 0; i < 4; i++) {
+            XWPFTableCell cell = headerRow.getCell(i);
+            XWPFParagraph paragraph = cell.getParagraphs().get(0);
+            paragraph.setAlignment(ParagraphAlignment.CENTER);
+            XWPFRun run = paragraph.createRun();
+            run.setBold(true); // Establecer negrita
+            switch (i) {
+                case 0:
+                    run.setText("Descripción");
+                    break;
+                case 1:
+                    run.setText("Código");
+                    break;
+                case 2:
+                    run.setText("Capitulo");
+                    break;
+                case 3:
+                    run.setText("Página");
+                    break;
+            }
+        }
+
+        int indexCategoria = 1;
+        for (Categoria category : reporte.getCategorias()) {
+            for (Seccion section : category.getSecciones()) {
+                XWPFTableRow row = summaryTable.createRow();
+                XWPFTableCell cell1 = row.getCell(0);
+                XWPFTableCell cell2 = row.getCell(1);
+                XWPFTableCell cell3 = row.getCell(2);
+
+                XWPFParagraph p1 = cell1.getParagraphs().get(0);
+                XWPFParagraph p2 = cell2.getParagraphs().get(0);
+                XWPFParagraph p3 = cell3.getParagraphs().get(0);
+
+                p1.setAlignment(ParagraphAlignment.LEFT);
+                p2.setAlignment(ParagraphAlignment.CENTER);
+                p3.setAlignment(ParagraphAlignment.CENTER);
+
+                XWPFRun r1 = p1.createRun();
+                XWPFRun r2 = p2.createRun();
+                XWPFRun r3 = p3.createRun();
+
+                r1.setText(section.getTitulo());
+                r2.setText("-");
+                r3.setText(Integer.toString(indexCategoria));
+            }
+            indexCategoria++;
         }
     }
 
