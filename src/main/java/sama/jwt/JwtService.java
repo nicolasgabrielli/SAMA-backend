a04dd8f5407a888a21eee5b08f3b31a82b00b26f
Ignacio Riquelme <ignacio.riquelme.r@usach.cl>
2024-08-07 21:03:20 -0400

    Cambio tiempo token

--- src/main/java/sama/jwt/JwtService.java
+++ src/main/java/sama/jwt/JwtService.java
@@ -36,7 +36,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*12))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*10))
                 .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
